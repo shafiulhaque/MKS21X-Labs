c7ef1ec6e79a966eb79cddb4b3ddbe95481c4ea0
Shafiul Haque <shafiulhaque@Shafiuls-MBP.lan>
2021-10-23 11:12:23 -0400

    adding the demo.java file

---
+++ 01-Object/Demo.java
@@ -0,0 +1,14 @@
+public class Demo{
+  /* more methods can go here*/
+
+  public static void main(String[]args){
+    Point P1 = new Point(1,1);
+    Point P2 = new Point(2,2);
+
+    System.out.println(  distance(P1,P2)); //call1
+    System.out.println(  Point.distance(P1,P2)); //call 2
+    System.out.println(  P1.distanceTo(P2)); //call 3
+    //This one is IMPLIED strongly. You cannot tell by the calling style alone,
+    //but you can make an educated guess.
+  }
+ }
