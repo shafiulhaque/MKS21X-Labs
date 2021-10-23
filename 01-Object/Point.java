25b2fc42b8452fc75f278cb86b3402ad96d75478
Shafiul Haque <shafiulhaque@Shafiuls-MBP.lan>
2021-10-23 10:58:09 -0400

    Created the point.java file

---
+++ 01-Object/Point.java
@@ -0,0 +1,17 @@
+public class Point{
+  private double x,y;
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+  public Point(Point p){
+    x= p.x;
+    y= p.y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+}
