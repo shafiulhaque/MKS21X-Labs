e823209992dfa85db52def7d01a04bfecda73837
Shafiul Haque <shafiulhaque@Shafiuls-MBP.lan>
2021-10-23 11:27:46 -0400

    Modified java files for first class function

--- 01-Object/Demo.java
+++ 01-Object/Demo.java
@@ -1,6 +1,11 @@
 public class Demo{
   /* more methods can go here*/
-
+  public static double distance(Point start, Point end){
+    double xcord = end.getX() - start.getX();
+    double ycord = end.getX() - start.getX();
+    double joe = Math.pow(xcord, 2) + Math.pow(ycord, 2);
+    return Math.sqrt(joe);
+  }
   public static void main(String[]args){
     Point P1 = new Point(1,1);
     Point P2 = new Point(2,2);
--- 01-Object/Point.java
+++ 01-Object/Point.java
@@ -1,13 +1,16 @@
 public class Point{
+  // One field (instance variable:
   private double x,y;
+  // Two constructors:
   public Point(double X, double Y){
     x=X;
     y=Y;
   }
   public Point(Point p){
-    x= p.x;
-    y= p.y;
+    x = p.x;
+    y = p.y;
   }
+  // Two accessor methods:
   public double getX(){
     return x;
   }
