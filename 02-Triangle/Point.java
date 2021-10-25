5ebcb24a60843c5a71457bd8456ff3c91566c379
Shafiul Haque <shafiulhaque@Shafiuls-MacBook-Pro.local>
2021-10-25 14:38:15 -0400

    Created 02-Triangle and started inputting code:

---
+++ 02-Triangle/Point.java
@@ -0,0 +1,33 @@
+public class Point{
+  // One field (instance variable:
+  private double x,y;
+  // Two constructors:
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+  public Point(Point p){
+    x = p.x;
+    y = p.y;
+  }
+  // Three accessor (non-static) methods:
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+  public double distanceTo(Point other){
+    double xcord = x - other.getX();
+    double ycord = y - other.getY();
+    double joe = Math.pow(xcord, 2) + Math.pow(ycord, 2);
+    return Math.sqrt(joe);
+  }
+  // One accessor (static) method:
+  public static double distance(Point start, Point end){
+    double xcord = end.getX() - start.getX();
+    double ycord = end.getY() - start.getY();
+    double joe = Math.pow(xcord, 2) + Math.pow(ycord, 2);
+    return Math.sqrt(joe);
+  }
+}
---
+++ 02-Triangle/Triangle.java
@@ -0,0 +1,6 @@
+public class Triangle{
+  // Two fields (instance variable):
+  private Point v1, v2, v3;
+  // Two constructors:
+  public 
+}
