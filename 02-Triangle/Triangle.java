3be056690d055576deff5d13fbe5b6e0041dfc99
Shafiul Haque <shafiulhaque@Shafiuls-MacBook-Pro.local>
2021-10-25 14:44:34 -0400

    updated file

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -2,5 +2,15 @@ public class Triangle{
   // Two fields (instance variable):
   private Point v1, v2, v3;
   // Two constructors:
-  public 
+  public Triangle(Point a, Point b, Point c){
+    v1 = a;
+    v2 = b;
+    v3 = c;
+  }
+  public Triangle(double a, double b, double c, double d, double e, double f){
+    v1 = new Point(a, b);
+    v2 = new Point(c, d);
+    v3 = new Point(e, f);
+  }
+  // Four accessor (non-static) methods:
 }
