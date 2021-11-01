e812e52d5a68700df0b8935adc900611e07e305d
Shafiul Haque <shafiulhaque@Shafiuls-MacBook-Pro.local>
2021-11-01 14:39:32 -0400

    adding 03 repo

---
+++ 03-GetReal/RealNumber.java
@@ -0,0 +1,48 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
+     //other can be ANY RealNumber, including a RationalNumber
+
+     //or other subclasses of RealNumber (that aren't written yet)
+
+     return 0;
+  }
+
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return 0;
+  }
+
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return 0;
+  }
+
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    return 0;
+  }
+
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+value;
+  }
+}
