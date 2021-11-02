47e8c012631ef56012465351cb87a8b112afa859
Shafiul Haque <shafiulhaque@Shafiuls-MBP.lan>
2021-11-01 22:00:56 -0400

    made more test stuff for real number

--- 03-GetReal/Tester.java
+++ 03-GetReal/Tester.java
@@ -1,4 +1,13 @@
 public class Tester{
   public static void main(String[] args) {
+    RealNumber joe = new RealNumber(43);
+    RealNumber joe2 = new RealNumber(34);
+    System.out.println(joe.getValue());
+    System.out.println(joe.add(joe2));
+    System.out.println(joe.multiply(joe2));
+    System.out.println(joe.subtract(joe2));
+    System.out.println(joe.divide(joe2));
+    System.out.println(joe.getValue());
+    System.out.println(joe);
   }
 }
