0e940521401e8deab56e2a90d400738fd9f7e5ef
Shafiul Haque <shafiulhaque@Shafiuls-MBP.lan>
2021-11-02 10:00:30 -0400

    adding test cases

--- 03-GetReal/Tester.java
+++ 03-GetReal/Tester.java
@@ -11,8 +11,9 @@ public class Tester{
     System.out.println(t1.getValue());
     System.out.println(t1);
     System.out.println("TEST CASES FOR RATIONAL NUMBER:");
-    RationalNumber t3 = new RationalNumber(23, 3);
+    RationalNumber t3 = new RationalNumber(8,6);
     RationalNumber t4 = new RationalNumber(16, 12);
+    RationalNumber t5 = new RationalNumber(21, 4);
     System.out.println(t3.getValue());
     System.out.println(t4.getValue());
     System.out.println(t3.getNumerator());
@@ -26,5 +27,20 @@ public class Tester{
     System.out.println(t3.divide(t4));
     System.out.println(t3.add(t4));
     System.out.println(t3.subtract(t4));
+    t3 = new RationalNumber(8,6);
+    System.out.println("---------------------");
+    System.out.println(t3.getValue());
+    System.out.println(t5.getValue());
+    System.out.println(t3.getNumerator());
+    System.out.println(t5.getNumerator());
+    System.out.println(t3.getDenominator());
+    System.out.println(t5.getDenominator());
+    System.out.println(t3.reciprocal());
+    System.out.println(t5.reciprocal());
+    System.out.println(t3.equals(t5));
+    System.out.println(t3.multiply(t5));
+    System.out.println(t3.divide(t5));
+    System.out.println(t3.add(t5));
+    System.out.println(t3.subtract(t5));
   }
 }
