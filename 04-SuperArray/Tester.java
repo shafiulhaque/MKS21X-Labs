386d4585094f2231fe0d9a9c3b1abe51e9dfbc6c
Shafiul Haque <shafiulhaque@Shafiuls-MBP.lan>
2021-11-04 21:39:49 -0400

    made a tester class

---
+++ 04-SuperArray/Tester.java
@@ -0,0 +1,11 @@
+public class Tester{
+  public static void main(String[] args) {
+    SuperArray joe = new SuperArray();
+    joe.add("one");
+    joe.add("two");
+    joe.add("three");
+    joe.add("four");
+    joe.add("five");
+    System.out.println(joe);
+  }
+}
