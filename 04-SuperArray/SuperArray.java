1fe8e91fe29e9cb6eccd597d6a5877af891d54f4
Shafiul Haque <shafiulhaque@Shafiuls-MacBook-Pro.local>
2021-11-05 14:30:16 -0400

    made phase two functions

--- 04-SuperArray/SuperArray.java
+++ 04-SuperArray/SuperArray.java
@@ -32,15 +32,31 @@ public class SuperArray{
     return joe + "]";
   }
 
+  public String get(int index){
+    return data[index];
+  }
+
+  public String set(int index, String element){
+    String x = data[index];
+    if (x == null){
+      System.out.println("Error: index is null");
+    } else {
+      data[index] = element;
+    }
+    return x;
+  }
+
   // mutator methods
-  public void add(String joe){
+  public boolean add(String joe){
     data[size] = joe;
     size++;
+    return true;
   }
   public void remove(int joe){
     for (int i = joe; i < data.length - 1; i++){
       data[i] = data[i+1];
     }
+    size--;
   }
 
 }
