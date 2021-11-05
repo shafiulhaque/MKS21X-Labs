70255160710e7902dd5b4c1d0b9b631374b6de59
Shafiul Haque <shafiulhaque@Shafiuls-MBP.lan>
2021-11-04 21:25:21 -0400

    made all the methods

--- 04-SuperArray/SuperArray.java
+++ 04-SuperArray/SuperArray.java
@@ -1,4 +1,41 @@
 public class SuperArray{
+  // fields
   private String[] data;
   private int size;
+
+  // constructors
+  public SuperArray(){
+    data = new String[10];
+    size = 0;
+  }
+
+  // accessor methods
+  public int size(){
+    return size;
+  }
+
+  public String toString(){
+    String joe = "[";
+    for (int i = 0; i < size; i++){
+      joe += data[i];
+      if (i <= size - 2) joe += ", ";
+    }
+    return joe + "]";
+  }
+
+  public String toStringDebug(){
+    String joe = "[";
+    for (int i = 0; i < data.length; i++){
+      joe += data[i];
+      if (i <= data.length - 2) joe += ", ";
+    }
+    return joe + "]";
+  }
+
+  // mutator methods
+  public void addString(String joe){
+    data[size] = joe;
+    size++;
+  }
+
 }
