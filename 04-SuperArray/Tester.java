9b50746ae38f172d0f82f8b6e7c1826ef39020c4
Shafiul Haque <shafiulhaque@Shafiuls-MacBook-Pro.local>
2021-11-05 14:30:38 -0400

    made get test cases

--- 04-SuperArray/Tester.java
+++ 04-SuperArray/Tester.java
@@ -1,13 +1,25 @@
 public class Tester{
   public static void main(String[] args) {
+    System.out.println("Phase One Test Cases");
     SuperArray joe = new SuperArray();
-    joe.add("one");
-    joe.add("two");
-    joe.add("three");
-    joe.add("four");
-    joe.add("five");
+    System.out.println(joe.add("one"));
+    System.out.println(joe.add("two"));
+    System.out.println(joe.add("three"));
+    System.out.println(joe.add("four"));
+    System.out.println(joe.add("five"));
+    System.out.println(joe.add("six"));
+    System.out.println(joe.add("seven"));
+    System.out.println(joe.add("eight"));
+    System.out.println(joe.add("nine"));
+    System.out.println(joe.add("ten"));
     System.out.println(joe);
     joe.remove(2);
     System.out.println(joe);
+
+    System.out.println("Phase Two Test Cases: Get");
+    for(int i = 0; i < 10; i++){
+      System.out.println(joe.get(i));
+    }
+
   }
 }
