07ded0b4f1dac29d9fdd6c70ed4c8fe45279c647
Shafiul Haque <shafiulhaque@Shafiuls-MBP.lan>
2021-11-10 21:21:19 -0500

    finished day2

--- AdventOfCode/Day2.java
+++ AdventOfCode/Day2.java
@@ -16,13 +16,16 @@ public class Day2{
     int a = Integer.parseInt(joe[0]);
     int b = Integer.parseInt(joe[1]);
     int c = Integer.parseInt(joe[2]);
+    int joe1 = a*b;
+    int joe2 = a*c;
+    int joe3 = c*b;
     int smol = 0;
-    if (a < b && a < c){
-      smol = a;
-    } else if (b < c){
-      smol = b;
+    if (joe1 < joe2 && joe1 < joe3){
+      smol = joe1;
+    } else if (joe2 < joe3){
+      smol = joe2;
     } else {
-      smol = c;
+      smol = joe3;
     }
     int wrap = 2*a*b + 2*b*c + 2*c*a + smol;
     return wrap;
