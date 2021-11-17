111b398012bb5065c5e2557905a792f5442b8fcf
Shafiul Haque <shaque30@stuy.edu>
2021-11-17 13:43:34 -0500

    made new file

---
+++ 05-ArrayList/ArrayListPractice.java
@@ -0,0 +1,14 @@
+public class ArrayListPractice{
+  public static ArrayList<String> replaceEmpty( ArrayList<String> data){
+  //return an new ArrayList that has all of the values of the original ArrayList except
+  //that all empty strings are replaced with the word "Empty".
+}
+  public static ArrayList<String> makeReversedList( ArrayList<String> data){
+  //return a new ArrayList that is in the reversed order of the parameter.
+}
+  public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+  //return a new ArrayList that has all values of a and b in alternating order that is:
+  //a[0], b[0], a[1], b[1]...
+  //when one list is longer than the other, just append the remaining values to the end.
+} 
+}
