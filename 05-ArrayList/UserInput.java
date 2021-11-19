f5339542aabdd21bb0536d5257ff56f6e441bb16
Shafiul Haque <shaque30@stuy.edu>
2021-11-19 18:46:34 -0500

    finished code

--- 05-ArrayList/UserInput.java
+++ 05-ArrayList/UserInput.java
@@ -8,11 +8,19 @@ public class UserInput{
       System.out.print("Enter a non negative number. A negative number will end the program:");
       num = Integer.parseInt(in.nextLine());
       numbers.add(num);
-        }
+    }
     in.close();
-    System.out.println("You entered:" + numbers);
-    System.out.println("Mean:" + )
-    System.out.println("Min value: " + )
-    System.out.println("Max value: " + )
+    int total = numbers.get(0);
+    int min = numbers.get(0);
+    int max = numbers.get(0);
+    for (int i = 1; i < numbers.size(); i++){
+      if (numbers.get(i) < min) min = numbers.get(i);
+      if (numbers.get(i) > max) max = numbers.get(i);
+      total += numbers.get(i);
+    }
+    System.out.println("You entered: " + numbers);
+    System.out.println("Mean: " + (double)total/numbers.size());
+    System.out.println("Min value: " + min);
+    System.out.println("Max value: " + max);
   }
 }
