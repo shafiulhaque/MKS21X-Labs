08f07f1a69637afe642e72fe53ec3ec807166777
Shafiul Haque <shaque30@stuy.edu>
2021-11-19 14:40:29 -0500

    made some changes

--- 05-ArrayList/ArrayListPractice.java
+++ 05-ArrayList/ArrayListPractice.java
@@ -3,20 +3,23 @@ public class ArrayListPractice {
   public static ArrayList<String> replaceEmpty( ArrayList<String> data){
     //return an new ArrayList that has all of the values of the original ArrayList except
     //that all empty strings are replaced with the word "Empty".
+    ArrayList<String> newData = new ArrayList<String>();
     for (int i = 0; i < data.size(); i++){
       if (data.get(i).equals("")){
-        data.set(i, "Empty");
+        newData.add("Empty");
+      } else {
+        newData.add(data.get(i));
       }
     }
-    return data;
+    return newData;
 }
   public static ArrayList<String> makeReversedList( ArrayList<String> data){
-  //return a new ArrayList that is in the reversed order of the parameter.
-  ArrayList<String> newData = new ArrayList<String>();
-  for (int i = data.size()-1; i >= 0; i--){
-    newData.add(data.get(i));
-  }
-  return newData;
+    //return a new ArrayList that is in the reversed order of the parameter.
+    ArrayList<String> newData = new ArrayList<String>();
+    for (int i = data.size()-1; i >= 0; i--){
+      newData.add(data.get(i));
+    }
+    return newData;
 }
   public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
   //return a new ArrayList that has all values of a and b in alternating order that is:
