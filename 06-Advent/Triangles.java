67662675938fa087e541b68a4ab483066180298b
Shafiul Haque <shaque30@stuy.edu>
2021-11-22 17:38:17 -0500

    added partOne and partTwo

--- 06-Advent/Triangles.java
+++ 06-Advent/Triangles.java
@@ -20,20 +20,40 @@ public class Triangles{
       return 1;
     }
   }
+  public static int partTwo(int a, int b, int c, int d, int e, int f, int g, int h, int i){
+    int total = 0;
+    total += partOne(a, d, g);
+    total += partOne(b, e, h);
+    total += partOne(c, f, i);
+    return total;
+  }
   public static void main(String[] args) {
     try {
       File theFile = new File("ReadFile.txt");
       Scanner input = new Scanner(theFile);
-      int total = 0;
+      int partOneTotal = 0;
+      int partTwoTotal = 0;
       while (input.hasNextInt()){
-        int x = input.nextInt();
-        int y = input.nextInt();
-        int z = input.nextInt();
-        total += partOne(x, y, z);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int d = input.nextInt();
+        int e = input.nextInt();
+        int f = input.nextInt();
+        int g = input.nextInt();
+        int h = input.nextInt();
+        int i = input.nextInt();
+        partOneTotal += partOne(a, b, c);
+        partOneTotal += partOne(d, e, f);
+        partOneTotal += partOne(g, h, i);
+        partTwoTotal += partTwo(a, b, c, d, e, f, g, h, i);
       }
-    System.out.println(total);
+    System.out.println(partOneTotal);
+    System.out.println(partTwoTotal);
     input.close();
   } catch (FileNotFoundException e) {
+    System.out.println("Exception: Cannot find file");
+    e.printStackTrace();
   }
 }
 }
