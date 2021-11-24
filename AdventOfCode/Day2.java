ed078d10545a9faa1b554b33015e06aa60b22c12
Shafiul Haque <shaque30@stuy.edu>
2021-11-24 08:33:51 -0500

    made some changes

--- AdventOfCode/Day2.java
+++ AdventOfCode/Day2.java
@@ -3,14 +3,9 @@ import java.io.*;
 
 public class Day2{
   public static String[] getNums(String joe){
-    String[] ok = new String[3];
-    ok[0] = joe.substring(0, joe.indexOf("x"));
-    joe = joe.substring(joe.indexOf("x")+1, joe.length());
-    ok[1] = joe.substring(0, joe.indexOf("x"));
-    joe = joe.substring(joe.indexOf("x")+1, joe.length());
-    ok[2] = joe;
+    String[] ok = joe.split("x");
     return ok;
-  }
+    }
 
   public static int getVals(String[] joe){
     int a = Integer.parseInt(joe[0]);
@@ -32,7 +27,7 @@ public class Day2{
   }
 
 public static void main(String[] args) throws Exception {
-  Scanner joe = new Scanner(new File("Day2gift.txt"));
+  Scanner joe = new Scanner(new File(args[0]));
   int total = 0;
   while (joe.hasNextLine()){
     total += getVals(getNums(joe.nextLine()));
