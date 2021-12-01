45b92419a7504298b39ee316fb81be6d482ef097
Shafiul Haque <shaque30@stuy.edu>
2021-12-01 13:50:24 -0500

    made test cases

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -5,6 +5,18 @@ public class Sorts{
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
+    
+  }
 
+  public static void main (String[] args){
+    int[] result = new int[]{3, 5, 2, 1};
+    bubbleSort(result);
+    System.out.println(result);
+    result = new int[]{2, 0, 4, 8};
+    bubbleSort(result);
+    System.out.println(result);
+    result = new int[]{5, 8, 423, 1, -6, -23, 5};
+    bubbleSort(result);
+    System.out.println(result);
   }
 }
