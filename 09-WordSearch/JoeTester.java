2d88a04df1c9a20933be9f6a828c4147242a327a
Shafiul Haque <shaque30@stuy.edu>
2021-12-09 14:48:29 -0500

    made more test cases

--- 09-WordSearch/JoeTester.java
+++ 09-WordSearch/JoeTester.java
@@ -1,10 +1,31 @@
 public class JoeTester{
   public static void main (String args[]){
     WordSearch joe = new WordSearch(6, 6);
+    System.out.println("\n" + "FIRST TEST");
     System.out.println(joe);
     System.out.println(joe.addWordHorizontal("joe", 4, 3));
+    System.out.println(joe);
     System.out.println(joe.addWordHorizontal("joe", 3, 4));
+    System.out.println(joe);
     System.out.println(joe.addWordVertical("joe", 4, 3));
+    System.out.println(joe);
     System.out.println(joe.addWordVertical("joe", 3, 4));
+    System.out.println(joe);
+    System.out.println("\n" + "NEXT TEST");
+    WordSearch no = new WordSearch(4, 6);
+    System.out.println(no);
+    System.out.println(no.addWordHorizontal("joe", 3, 2));
+    System.out.println(no);
+    System.out.println(no.addWordHorizontal("joe", 2, 3));
+    System.out.println(no);
+    System.out.println(no.addWordVertical("joe", 3, 2));
+    System.out.println(no);
+    System.out.println(no.addWordVertical("joe", 1, 4));
+    System.out.println(no);
+    System.out.println("\n");
+    System.out.println(no.addWordDiagonal("joe", 1, 3));
+    System.out.println(no);
+
+
   }
 }
