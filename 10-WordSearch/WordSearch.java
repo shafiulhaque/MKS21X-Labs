15d33b110a77510a767b420094d7c259be2f4434
Shafiul Haque <shaque30@stuy.edu>
2021-12-14 14:58:41 -0500

    made more tries

--- 10-WordSearch/WordSearch.java
+++ 10-WordSearch/WordSearch.java
@@ -124,7 +124,7 @@ public class WordSearch{
          int row, col, rowInc, colInc;
          boolean addedWord = false;
          String str = wordsToBeAdded.remove(randgen.nextInt(wordsToBeAdded.size()));
-         while (tries < 10 && !addedWord){
+         while (tries < 100 && !addedWord){
            // System.out.println(joe);
            rowInc = dir[randgen.nextInt(dir.length)];
            colInc = dir[randgen.nextInt(dir.length)];
@@ -180,5 +180,20 @@ public class WordSearch{
       }
       str += "\n" + "Seed: " + this.seed;
       return str;
+    }
+
+    public static void main (String args[]){
+      int row = Integer.parseInt(args[0]);
+      int col = Integer.parseInt(args[1]);
+      String txt = args[2];
+      int mode = Integer.parseInt(args[3]);
+      if (args.length < 5){
+        WordSearch os = new WordSearch(row, col, txt, mode);
+        System.out.println(os);
+      } else{
+        int seed = Integer.parseInt(args[4]);
+        WordSearch os = new WordSearch(row, col, txt, mode, seed);
+        System.out.println(os);
+      }
     }
 }
