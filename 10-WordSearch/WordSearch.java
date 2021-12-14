bc01bb8ddca6a24a33f3547eadf0001a5632f2d6
Shafiul Haque <shaque30@stuy.edu>
2021-12-14 14:29:59 -0500

    removed useless stuff

--- 10-WordSearch/WordSearch.java
+++ 10-WordSearch/WordSearch.java
@@ -67,28 +67,33 @@ public class WordSearch{
     *        b) the word doesn't fit,
     *        c) there are overlapping letters that do not match
     */
-    public boolean addWord(int row, int col, String word, int rowInc, int colInc){
-      int wordLen = word.length();
-      if (rowInc == 0 && colInc == 0){
-        //System.out.println("rowInc and colInc won't move");
-        return false;
+      //precondition word grids are rectangular
+      private boolean inBounds(int r, int c){
+        return r >= 0 && r < data.length && c >= 0 && c < data[0].length;
       }
-      try {
-        for (int i = 0; i < wordLen; i++){
-          if (data[row+rowInc*i][col+colInc*i] != '_' && data[row+rowInc*i][col+colInc*i] != word.charAt(i)){
-            //System.out.println("Word overlaps with other letters (RIP)");
+      public boolean addWord(String word,int row, int col, int dr, int dc){
+        //make sure there is some non 0 vector for direction
+        if(dr == 0 && dc == 0){
+          return false;
+        }
+        //check endpoints of word
+        if(!inBounds(row,col) || !inBounds(row + dr * (word.length() - 1), col + dc * (word.length() - 1 ))){
+          return false;
+        }
+        //check for conflicting letters
+        for(int i = 0; i < word.length(); i++){
+          char c = data[row + i * dr][col + i * dc];
+          if(c != '_' && c != word.charAt(i)){
             return false;
           }
         }
-      } catch(IndexOutOfBoundsException e){
-        //System.out.println("Index is out of bounds, which means word does not fit (RIP) ");
-        return false;
+        //change
+        for(int i = 0; i < word.length(); i++){
+          data[row + i * dr][col + i * dc] = word.charAt(i);
+        }
+        wordsAdded.add(word);
+        return true;
       }
-      for (int i = 0; i < wordLen; i++){
-        data[row+rowInc*i][col+colInc*i] = word.charAt(i);
-      }
-      return true;
-    }
 
     /*Attempt to add all of the words from the file of words list using the algorithm described above
      *Suggestion: read your file into an ArrayList of Strings. */
@@ -125,8 +130,7 @@ public class WordSearch{
            colInc = dir[randgen.nextInt(dir.length)];
            row = randgen.nextInt(data.length);
            col = randgen.nextInt(data[0].length);
-           if (addWord(row, col, str, rowInc, colInc) == true){
-             addWord(row, col, str, rowInc, colInc);
+           if (addWord(str, row, col, rowInc, colInc) == true){
              wordsAdded.add(str);
              addedWord = true;
            }
@@ -179,94 +183,4 @@ public class WordSearch{
       str += "\n" + "Seed: " + this.seed;
       return str;
     }
-
-// OLD METHODS
-
-    /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from left to right, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical locaiton of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     * or there are overlapping letters that do not match, then false is returned
-     * and the board is NOT modified.
-    // public boolean addWordHorizontal(String word, int row, int col){
-    //   int wordLength = word.length();
-    //   if (wordLength > data[1].length - col){
-    //     System.out.println("Word cannot fit in wordsearch (RIP)");
-    //     return false;
-    //   }
-    //   for (int i = 0; i < wordLength; i++){
-    //     if (data[row][col+i] != '_' && data[row][col+i] != word.charAt(i)){
-    //       System.out.println("Word overlapps with other letters (RIP)");
-    //       return false;
-    //     }
-    //   }
-    //   for (int i = 0; i < wordLength; i++){
-    //    data[row][col+i] = word.charAt(i);
-    //  }
-    //   return true;
-    // }
-    */
-
-   /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from top to bottom, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical locaiton of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     *or there are overlapping letters that do not match, then false is returned.
-     *and the board is NOT modified.
-
-    // public boolean addWordVertical(String word, int row, int col){
-    //   int wordLength = word.length();
-    //   if (wordLength > data.length - row){
-    //     System.out.println("Word cannot fit in wordsearch (RIP)");
-    //     return false;
-    //   }
-    //   for (int i = 0; i < wordLength; i++){
-    //     if (data[row+i][col] != '_' && data[row+i][col] != word.charAt(i)){
-    //       System.out.println("Word overlapps with other letters (RIP)");
-    //       return false;
-    //     }
-    //   }
-    //   for (int i = 0; i < wordLength; i++){
-    //     data[row+i][col] = word.charAt(i);
-    //   }
-    //   return true;
-    // }
-    */
-
-    /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from top left towards the bottom right, it must fit on the board,
-     *and must have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical locaiton of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     *or there are overlapping letters that do not match, then false is returned
-     *and the board is not modified.
-    // public boolean addWordDiagonal(String word, int row, int col){
-    //   int wordLength = word.length();
-    //   if (data.length - row < wordLength || data[1].length - col < wordLength){
-    //     System.out.println("Word cannot fit in wordsearch (RIP)");
-    //     return false;
-    //   }
-    //   for (int i = 0; i < wordLength; i++){
-    //     if (data[row+i][col+i] != '_' && data[row+i][col+i] != word.charAt(i)){
-    //       System.out.println("Word overlapps with other letters (RIP)");
-    //       return false;
-    //     }
-    //   }
-    //   for (int i = 0; i < wordLength; i++){
-    //     data[row+i][col+i] = word.charAt(i);
-    //   }
-    //   return true;
-    // }
-    */
 }
