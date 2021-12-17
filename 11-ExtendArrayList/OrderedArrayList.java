43eef2d3b3deccb877d65d54bd2e2d4dfc20644c
Shafiul Haque <shaque30@stuy.edu>
2021-12-17 15:15:05 -0500

    finished both add and a set method

--- 11-ExtendArrayList/OrderedArrayList.java
+++ 11-ExtendArrayList/OrderedArrayList.java
@@ -13,6 +13,31 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   private int whereToPlace(T value){
       /*return the index that the value should be placed
       when inserting into the OrderedArrayList .*/
-      return 0;
-    }
+      int index = super.size()-1;
+      for (int i = 0; i < super.size(); i++){
+        if (value.compareTo(super.get(i)) < 0){
+          index = i;
+          break;
+        }
+      }
+      return index;
+  }
+
+  public boolean add(T value){
+    int index = whereToPlace(value);
+    super.add(index, value);
+    return true;
+  }
+
+  public void add(int index, T value){
+    int ind = whereToPlace(value);
+    super.add(ind, value);
+  }
+
+  public T set(int index, T value){
+    T element = super.remove(index);
+    add(value);
+    return element;
+  }
+
 }
