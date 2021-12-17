e54c7f2a8083cbdaa88ec02ac54d52c6f5567848
Shafiul Haque <shaque30@stuy.edu>
2021-12-17 14:15:41 -0500

    modified add and set methods

--- 11-ExtendArrayList/NoNullArrayList.java
+++ 11-ExtendArrayList/NoNullArrayList.java
@@ -6,8 +6,8 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     super();
   }
 
-  public NoNullArrayList(T startingCapacity){
-    super();
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
   }
 
   public T set(int index, T element){
