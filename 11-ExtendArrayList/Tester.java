08fe184061789ffff2de89cce047519c6f3f9995
Shafiul Haque <shaque30@stuy.edu>
2021-12-17 15:20:43 -0500

    made some test cases for no null array list

--- 11-ExtendArrayList/Tester.java
+++ 11-ExtendArrayList/Tester.java
@@ -3,6 +3,43 @@ import java.io.*;
 
 public class Tester {
    public static void main(String[] args) {
+     try {
+       NoNullArrayList<String> nonullreg = new NoNullArrayList<String>();
+       nonullreg.add("christmas");
+       System.out.println(nonullreg);
+       nonullreg.add("tree");
+       System.out.println(nonullreg);
+       nonullreg.add(1, "star");
+       System.out.println(nonullreg);
+       nonullreg.add(0, "light");
+       System.out.println(nonullreg);
+       nonullreg.set(0,"no");
+       System.out.println(nonullreg);
+       nonullreg.set(0,null);
+       System.out.println(nonullreg);
+     }
+     catch (IllegalArgumentException e){
+       System.out.println(e);
+     }
+     try {
+       NoNullArrayList<Integer> nonullint = new NoNullArrayList<Integer>(10);
+       nonullint.add(4);
+       System.out.println(nonullint);
+       nonullint.add(0,33);
+       System.out.println(nonullint);
+       nonullint.add(42);
+       System.out.println(nonullint);
+       nonullint.add(-11);
+       System.out.println(nonullint);
+       nonullint.set(1, -323);
+       System.out.println(nonullint);
+       nonullint.set(0, null);
+       System.out.println(nonullint);
+     } catch (IllegalArgumentException e){
+       System.out.println(e);
+     }
+
      
+
+     }
    }
- }
