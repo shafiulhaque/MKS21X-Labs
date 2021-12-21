e97b3cded5dac9fb380ba1e68d6399ceea01a36d
Shafiul Haque <shaque30@stuy.edu>
2021-12-21 14:44:14 -0500

    made a new file

---
+++ ClassWork/StringToInt.java
@@ -0,0 +1,9 @@
+
+
+public class StringToInt{
+  public static int stringToInt(String s){
+     //return the value of Integer.parseInt(s) but do it yourself!
+     //Do not use any built in parse methods.
+     return 0;
+   }
+}
