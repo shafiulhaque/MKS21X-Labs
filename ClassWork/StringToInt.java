9aecd6fde04bac418b72bd055e37ce4aed57889f
Shafiul Haque <shaque30@stuy.edu>
2021-12-31 22:06:28 -0500

    finished making function work with negative numbers, happy new years

--- ClassWork/StringToInt.java
+++ ClassWork/StringToInt.java
@@ -3,16 +3,24 @@ public class StringToInt{
   //Do not use any built in parse methods.
   public static int stringToInt(String s){
     int stringInt = 0;
+    boolean isNeg = false;
+    if (s.charAt(0) == '-'){
+      s = s.substring(1);
+      isNeg = true;
+    }
      for (int i = 0; i < s.length(); i++){
        char cr = s.charAt(i);
        stringInt *= 10;
        stringInt += valueOfDigit(cr);
      }
+     if (isNeg) stringInt *= -1;
      return stringInt;
    }
+
    public static int valueOfDigit(char c){
      return c - '0';
    }
+
    public static void main(String args[]){
      System.out.println(stringToInt("23232"));
      System.out.println(stringToInt("111"));
