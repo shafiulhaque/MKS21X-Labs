f78a22410cd7e36d850bc1922e84887ccf6cc1b8
Shafiul Haque <shaque30@stuy.edu>
2022-01-07 14:47:04 -0500

    finished making wizard, will change later

--- 12-Stuyablo/Game.java
+++ 12-Stuyablo/Game.java
@@ -13,6 +13,18 @@ public class Game{
     System.out.println(a+" ("+a.getHP()+"HP)");
     System.out.println(b+" ("+b.getHP()+"HP)");
 
+    Adventurer c = new Wizard("Generic Name","Aaaaaaaar!",20);
+    Adventurer d = new Wizard("Homer","Donuts?",0);
+    System.out.println(c+" ("+c.getHP()+"HP)");
+    System.out.println(d+" ("+d.getHP()+"HP)");
+    a.attack(b);
+    b.attack(a);
+    System.out.println(a+" ("+a.getHP()+"HP)");
+    System.out.println(b+" ("+b.getHP()+"HP)");
+    a.specialAttack(b);
+    b.specialAttack(a);
+    System.out.println(a+" ("+a.getHP()+"HP)");
+    System.out.println(b+" ("+b.getHP()+"HP)");
     }
 
 }
--- 12-Stuyablo/Wizard.java
+++ 12-Stuyablo/Wizard.java
@@ -15,4 +15,44 @@ public class Wizard extends Adventurer {
     setWarcry(warcry);
     setMana(mana);
   }
+
+  // wizard Methods
+
+  public void attack(Damageable other){
+      int damage = (int)(Math.random()*8)+1;
+      other.applyDamage(damage);
+      setMana(getMana() + 1);
+      System.out.println(this +
+          " attacked " + other + " for " +
+          damage + " damage!");
+  }
+
+  public void specialAttack(Damageable other){
+     if(getMana() >= 5){
+          int damage = (int)(Math.random()*16)+1;
+          other.applyDamage(damage);
+          System.out.println(this + " unleashes his wrath upon "
+           + other + " for " + damage + " damage! "+warcry);
+          setMana(getMana() - 5);
+    }else{
+        System.out.println("Not enough rage! ");
+        attack(other);
+    }
+  }
+
+  //get methods
+
+  public int getMana(){
+     return mana;
+  }
+
+  //set methods
+  public void setMana(int m){
+     this.mana = m;
+  }
+
+  public void setWarcry(String warcry){
+     this.warcry = warcry;
+  }
+
 }
