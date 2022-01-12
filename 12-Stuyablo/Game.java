9c7fea80083539a40ec56e4656d8cb4c0e3df892
Shafiul Haque <shaque30@stuy.edu>
2022-01-12 14:50:52 -0500

    made changes, finished

--- 12-Stuyablo/Game.java
+++ 12-Stuyablo/Game.java
@@ -1,17 +1,45 @@
+import java.util.*;
 public class Game{
   public static void main(String[] args) {
     TerminalCodes.hideCursor();
     TerminalCodes.clear();
-    int[] no = new int[4];
 
+    int[] no = new int[4];
     for(int i = 0; i < 4; i++){
       no[i] = (int)(Math.random()*99)+1;
     }
+
     TerminalCodes.inputNo(no);
     TerminalCodes.makeRow();
 
-     TerminalCodes.go(31,1);
-     TerminalCodes.reset();
-     TerminalCodes.showCursor();
+    TerminalCodes.go(31,1);
+    System.out.print(">");
+
+    Scanner in = new Scanner(System.in);
+    String joe = in.nextLine();
+    while(!joe.equals("q") && !joe.equals("quit")){
+      TerminalCodes.clear();
+      if (joe == ""){
+        TerminalCodes.inputNo(no);
+        TerminalCodes.makeRow();
+        TerminalCodes.go(31,1);
+        System.out.print(">");
+        joe = in.nextLine();
+      } else {
+        TerminalCodes.clear();
+        no = new int[4];
+        for(int i = 0; i < 4; i++){
+          no[i] = (int)(Math.random()*99)+1;
+        }
+        TerminalCodes.inputNo(no);
+        TerminalCodes.makeRow();
+        TerminalCodes.go(31,1);
+        System.out.print(">");
+        joe = in.nextLine();
+      }
+    }
+    in.close();
+    TerminalCodes.reset();
+    TerminalCodes.showCursor();
   }
 }
