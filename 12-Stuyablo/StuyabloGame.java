32892658f12ff7de799a79ed3d6808c5c2a65d68
Shafiul Haque <shaque30@stuy.edu>
2022-01-13 14:17:17 -0500

    adding new files to the repository

---
+++ 12-Stuyablo/StuyabloGame.java
@@ -0,0 +1,136 @@
+import java.util.*;
+public class StuyabloGame{
+  private static final int WIDTH = 80;
+  private static final int HEIGHT = 30;
+  private static final int BORDER_COLOR = Text.BLACK;
+  private static final int BORDER_BACKGROUND = Text.WHITE + Text.BACKGROUND;
+
+  public static void main(String[] args) {
+    run();
+  }
+
+  //Display a List of 1-4 adventurers on the rows row through row+3 (4 rows max)
+  //Should include Name and HP on 2 separate lines. (more to be added later)
+  public static void drawParty(ArrayList<Adventurer> party,int startRow){
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+    //YOUR CODE HERE
+    /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+  }
+
+  //Display a line of text starting at column 2 of the specified row.
+  public static void drawText(String s,int startRow){
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+    //YOUR CODE HERE
+    /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+  }
+
+  public static void drawScreen(){
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+    //YOUR CODE HERE
+    /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+  }
+
+
+  public static void run(){
+    //Clear and initialize
+    Text.hideCursor();
+    Text.clear();
+    Text.go(1,1);
+
+
+    //Things to attack:
+    //Make an ArrayList of Adventurers and add 1 enemy to it.
+    ArrayList<Adventurer>enemies = new ArrayList<>();
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+    //YOUR CODE HERE
+    /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+
+    //Adventurers you control:
+    //Make an ArrayList of Adventurers and add 3 Adventurers to it.
+    ArrayList<Adventurer> party = new ArrayList<>();
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+    //YOUR CODE HERE
+    /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+
+    //Main loop
+    boolean partyTurn = false;
+    int whichPlayer = 0;
+    int turn = 0;
+
+
+    String input = "";
+    Scanner in = new Scanner(System.in);
+    while(! (input.equalsIgnoreCase("q") || input.equalsIgnoreCase("quit"))){
+
+      //Draw the window border
+      Text.hideCursor();
+      drawScreen();
+
+      //display event based on last turn's input
+      if(partyTurn){
+        //Process user input:
+        if(input.equals("attack")){
+          /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+          //YOUR CODE HERE
+          /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+        }
+        else if(input.equals("special")){
+          /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+          //YOUR CODE HERE
+          /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+        }
+        whichPlayer++;
+
+
+
+        if(whichPlayer < party.size()){
+          drawText("Enter command for "+party.get(whichPlayer)+
+                   ": attack/special/quit",HEIGHT/2);
+        }else{
+          drawText("press enter to see monster's turn",HEIGHT/2);
+          partyTurn = false;
+        }
+      }else{
+        //this block ignores user input!
+        //display enemy attack except on turn 0.
+        if(turn > 0){
+          //Enemy action choices go here!
+          /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+          //YOUR CODE HERE
+          /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+        }
+
+        //after enemy goes, change back to player's turn.
+        partyTurn=true;
+        whichPlayer = 0;
+        //display which player's turn is next and prompt for action.
+        drawText("Enter command for "+party.get(whichPlayer)+": attack/special/quit",HEIGHT/2);
+
+        //end the turn.
+        turn++;
+
+      }
+
+      //display current state of all Adventurers
+      drawParty(party,2);
+      drawParty(enemies,HEIGHT-5);
+
+      //Draw the prompt
+      Text.reset();
+      Text.go(HEIGHT+1,1);
+      Text.showCursor();
+      System.out.print(">");
+      //Read user input
+      input = in.nextLine();
+    }
+
+    //After quit reset things:
+    Text.reset();
+    Text.showCursor();
+    Text.go(32,1);
+  }
+
+
+
+
+}
