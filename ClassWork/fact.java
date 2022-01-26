36d287253b2e85b2ad7e05b8bfc25fbd1dcf4c19
Shafiul Haque <shaque30@stuy.edu>
2022-01-26 14:49:40 -0500

    finished isEven function

--- ClassWork/fact.java
+++ ClassWork/fact.java
@@ -6,9 +6,21 @@ public class fact{
       return 1;
     }
   }
+
+  public static boolean isEven(int n){
+    if (n > 2){
+      return isEven(n-2);
+    } else {
+      if (n < 0) return isEven(n+2);
+      if (n == 1) return false;
+      return true;
+    }
+  }
+
   public static void main(String args[]){
     System.out.println(factorial(1));
     System.out.println(factorial(5));
     System.out.println(factorial(10));
+    System.out.println(isEven(-232));
   }
 }
